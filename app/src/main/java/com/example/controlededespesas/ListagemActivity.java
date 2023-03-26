16f0c3e4bef01471ea022cabafe06aa57d219fe8
AristotelesDantas <arisdan@hotmail.com>
2023-03-26 15:12:24 -0300

    26/03/23

--- app/src/main/java/com/example/controlededespesas/ListagemActivity.java
+++ app/src/main/java/com/example/controlededespesas/ListagemActivity.java
@@ -2,25 +2,77 @@ package com.example.controlededespesas;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ListagemActivity extends AppCompatActivity {
 
     private ListView listViewDespesas;
     private ArrayList<Despesa> listDespesas;
-    private ArrayAdapter<Despesa> listaAdapter;
+    private ArrayAdapter listaAdapter;
 
+    private androidx.appcompat.view.ActionMode actionMode;
     private int posicaoSelecionada = -1;
+    private View viewSelecionada;
+
+       private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.editar_excluir, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+            switch (item.getItemId()){
+                case R.id.menuItemEditar:
+                    alterarDespesa();
+                    mode.finish();
+                    return true;
+
+                case R.id.menuItemExcluir:
+                    excluirDespesa();
+                    mode.finish();
+                    return true;
+
+                default: return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+
+            if(viewSelecionada != null){
+                viewSelecionada.setBackgroundColor(Color.TRANSPARENT);
+            }
+
+            actionMode = null;
+            viewSelecionada = null;
+
+            listViewDespesas.setEnabled(true);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,85 +82,73 @@ public class ListagemActivity extends AppCompatActivity {
         listViewDespesas = findViewById(R.id.listViewDespesas);
 
         listViewDespesas.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
+                new AdapterView.OnItemClickListener(){
+
                     @Override
                     public void onItemClick(AdapterView<?> parent,
-                                            View view, int position,
-                                            long id) {
+                                            View view, int position, long id) {
+
                         posicaoSelecionada = position;
                         alterarDespesa();
                     }
-                });
-
-        listViewDespesas.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView,
-                                           View view, int position, long id) {
-                posicaoSelecionada = position;
-                alterarDespesa();
-                return true;
-            }
         });
 
+        listViewDespesas.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        listViewDespesas.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent,
+                                                   View view, int position,
+                                                   long id) {
+                        if (actionMode != null) {
+                            return false;
+                        }
+
+                        posicaoSelecionada = position;
+
+                        view.setBackgroundColor(Color.LTGRAY);
+
+                        viewSelecionada = view;
+
+                        listViewDespesas.setEnabled(false);
+
+                        actionMode = startSupportActionMode(mActionModeCallback);
+
+                        return true;
+                    }
+                });
         popularLista();
-    }
+        }
 
     private void popularLista() {
 
         listDespesas = new ArrayList<>();
 
-        listaAdapter = new ArrayAdapter<>(this,
-                                            android.R.layout.simple_list_item_1,
-                                            listDespesas);
+        listaAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, listDespesas);
 
         listViewDespesas.setAdapter(listaAdapter);
 
-
-
-
-        /*String[] categoria = getResources().getStringArray(R.array.categoria);
-        String[] descricao = getResources().getStringArray(R.array.descricao);
-        String[] conta = getResources().getStringArray(R.array.conta);
-        int[] pagamento = getResources().getIntArray(R.array.pagamento);
-        String[] valor = getResources().getStringArray(R.array.valor);
-
-        despesas = new ArrayList<>();
-
-        Despesa despesa;
-        float val;
-
-        Tipo[] tipos = Tipo.values();
-
-        for (int cont = 0; cont < categoria.length; cont++) {
-
-            despesa = new Despesa(categoria[cont]);
-            despesa.setDescricao(descricao[cont]);
-            despesa.setConta(conta[cont]);
-            despesa.setPagamento(tipos[pagamento[cont]]);
-
-            val = Float.parseFloat(valor[cont]);
-
-            despesa.setValor(val);
-            despesas.add(despesa);
         }
 
-        DespesaAdapter despesaAdapter = new DespesaAdapter(this, despesas);
+        private void excluirDespesa() {
+        listDespesas.remove(posicaoSelecionada);
+        listaAdapter.notifyDataSetChanged();
+        }
 
-        listViewDespesas.setAdapter(despesaAdapter);
-    }*/
-    }
+        private void alterarDespesa(){
 
-    private void alterarDespesa(){
         Despesa despesa = listDespesas.get(posicaoSelecionada);
-
         MainActivity.alterarDespesa(this, despesa);
-    }
+        }
 
-    public void adicionarDespesa(View view){
+    public void adicionarDespesa(){
         MainActivity.novaDespesa(this);
     }
 
-    public void abrirAutoria(View view){
+    public void abrirAutoria(){
         Autoria.sobre(this);
     }
 
@@ -149,6 +189,28 @@ public class ListagemActivity extends AppCompatActivity {
                 listDespesas.add(despesa);
             }
             listaAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.principal_opcoes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuItemAdicionar:
+                adicionarDespesa();
+               return true;
+
+            case R.id.menuItemSobre:
+                abrirAutoria();
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
         }
     }
 }
--- app/src/main/java/com/example/controlededespesas/MainActivity.java
+++ app/src/main/java/com/example/controlededespesas/MainActivity.java
@@ -4,12 +4,15 @@ import static com.example.controlededespesas.R.id.checkBoxCarteira;
 import static com.example.controlededespesas.R.id.checkBoxPoupanca;
 import static com.example.controlededespesas.R.id.radioGroupCategoria;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
@@ -46,8 +49,6 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox cbCarteira, cbContaCorrente, cbPoupanca;
     private EditText editValor, editDescricao;
 
-
-
     private int modo;
 
     public static void novaDespesa(AppCompatActivity activity) {
@@ -77,6 +78,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         radioGroupCategoria = findViewById(R.id.radioGroupCategoria);
         editDescricao       = findViewById(R.id.textViewDescri);
@@ -155,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void salvar(View view){
+    public void salvar(){
         int categoria;
 
         switch (radioGroupCategoria.getCheckedRadioButtonId()){
@@ -229,7 +235,7 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    public void cancelar(View view){
+    public void cancelar(){
         onBackPressed();
     }
 
@@ -238,151 +244,26 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-
-
-
-
-
-
-     /*   dp = findViewById(R.id.editTextDate);
-        Calendar calendar = Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int i2) {
-
-                calendar.set(Calendar.YEAR, year);
-                calendar.set(Calendar.MONTH, month);
-                Locale locale = new Locale("PT", "BR");
-                updateCalendar();
-
-            }
-            private void updateCalendar(){
-                String Format = "LLLL";
-                SimpleDateFormat sdf = new SimpleDateFormat(Format, Locale.US);
-
-                dp.setText(sdf.format(calendar.getTime()));
-            }
-        };
-        dp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new DatePickerDialog(MainActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),calendar.get(Calendar.DATE)).show();
-            }
-        });
-
-
-
-        popularSpinner();
-
-          numberFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.despesa_opcoes, menu);
+        return true;
     }
 
-    public void popularSpinner(){
-        ArrayList<String> lista = new ArrayList<>();
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuItemSalvar:
+                salvar();
+                return true;
 
-        lista.add(getString(R.string.dinheiro));
-        lista.add(getString(R.string.cartao));
-        lista.add(getString(R.string.pix));
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista);
-
-        spinnerPagamento.setAdapter(adapter);
-    }
-
-    public void mostrarSelecionados (View view){
-
-        //--------------------Mostrar as despesas------------------
-
-          String mensagem1 = "";
-        switch (radioGroupDespesas.getCheckedRadioButtonId()){
-            case R.id.radioButtonFixas:
-                mensagem1 = getString(R.string.despesas_fixas) +
-                           getString(R.string.foi_selecionada);
-                break;
-            case R.id.radioButtonAlimentacao:
-                mensagem1 = getString(R.string.despesas_com_alimentacao) +
-                           getString(R.string.foi_selecionada);
-                break;
-            case R.id.radioButtonTransportes:
-                mensagem1 = getString(R.string.despesas_com_transporte) +
-                           getString(R.string.foi_selecionada);
-                break;
-            case R.id.radioButtonLazer:
-                mensagem1 = getString(R.string.despesas_com_lazer) +
-                           getString(R.string.foi_selecionada);
-                break;
+            case android.R.id.home:
+            case R.id.menuItemCancelar:
+                cancelar();
+                return true;
 
             default:
-                mensagem1 = getString(R.string.nenhuma_despesa_selecionada);
+                return super.onOptionsItemSelected(item);
         }
-        //Toast.makeText(this,                mensagem1,                Toast.LENGTH_LONG).show();
-
-
-        //-------------------Mostrar as contas-----------------
-
-        String mensagem2 = "";
-
-        if (cbCarteira.isChecked()){
-            mensagem2 = getString(R.string.carteira) + "\n";
-        }
-        if (cbContaCorrente.isChecked()){
-            mensagem2 = getString(R.string.conta_corrente);
-        }
-        if (cbPoupanca.isChecked()){
-            mensagem2 = getString(R.string.poupanca);
-        }
-        if (mensagem2.equals("")){
-            mensagem2 = getString(R.string.nenhuma_opcao_foi_selecionada);
-        }else{
-            mensagem2 = getString(R.string.foram_selecionados) + "\n" + mensagem2;
-        }
-        //Toast.makeText(this, mensagem2, Toast.LENGTH_LONG).show();
-
-
-
-        //------------------Mostrar forma de pagamento--------------
-
-        String ling = (String) spinnerPagamento.getSelectedItem();
-
-        String mensagem;
-
-        if (ling != null){
-            mensagem = ling + " " + getString(R.string.foi_selecionado);
-        }else{
-            mensagem = getString(R.string.nenhum_selecionado);
-        }
-       // Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show();
-
-        //------------------Mostrar Valor pago--------------
-
-        int result = Integer.parseInt(editValor.getText().toString());
-        String mensagem3;
-
-        if (result != 0){
-            mensagem3 = result + " " + "Reais";
-        }else{
-            mensagem3 = getString(R.string.valor_nulo);
-        }
-
-        Toast.makeText(this, mensagem1 + "\n" + mensagem2 + "\n" + mensagem + "\n" + mensagem3, Toast.LENGTH_LONG).show();
-
     }
-
-    public void limparTudo (View view){
-        //------------Limpar despesas---------
-        radioGroupDespesas.clearCheck();
-
-        //------------Limpar contas-----------
-        cbCarteira.setChecked(false);
-        cbContaCorrente.setChecked(false);
-        cbPoupanca.setChecked(false);
-
-
-        //------------Limpar Valor-----------
-        editValor.setText("");
-
-    }
-
-*/
-
 }
